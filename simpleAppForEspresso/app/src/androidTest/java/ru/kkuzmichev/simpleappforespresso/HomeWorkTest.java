a14a8ff4d387a60584e530dd50268009218cc212
Sergey Scherbakov <sherbakov_s_a@mail.ru>
2022-10-07 21:36:26 +0300

    complited homework

---
+++ simpleAppForEspresso/app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/HomeWorkTest.java
@@ -0,0 +1,33 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+
+@RunWith(AllureAndroidJUnit4.class)
+public class HomeWorkTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testCheckTextOnHomeScreen() {
+        ViewInteraction mainText = onView(
+                ViewMatchers.withId(R.id.text_home)
+        );
+        mainText.check(
+                matches(
+                        withText("This is home fragment")));
+    }
+}
